89aba8cba4294d7c976867c58dcb44a7afb49eb1
Santiago Gabriel Vollmar <santiago.vollmar@gmail.com>
2018-04-04 10:12:35 +0200

    Documented LogicManager

--- src/ch/santiagovollmar/gol/logic/LogicManager.java
+++ src/ch/santiagovollmar/gol/logic/LogicManager.java
@@ -6,19 +6,35 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Handles the game logic
+ */
 public class LogicManager {
     private static boolean isPaused = false;
 
     private static volatile boolean updating;
 
+    /**
+     * Returns whether or not the game is currently updating
+     *
+     * @return
+     */
     public static boolean isUpdating() {
         return updating;
     }
 
     public static volatile long sleepTime;
 
-    static {setPercentageSleepTime(50);}
+    static {
+        setPercentageSleepTime(50);
+    }
 
+    /**
+     * Sets the time to sleep between two generations. The sleep time is calculated as follows:
+     * <code> 1000ms * (percentage / 100d)</code>
+     *
+     * @param percentage A number between 0 and 100
+     */
     public static void setPercentageSleepTime(int percentage) {
         // ensure bounds
         percentage = percentage > 100 ? 100 : percentage;
@@ -30,6 +46,10 @@ public class LogicManager {
         }
     }
 
+    /**
+     * Invokes {@link GridManager#consume()} and {@link GridManager#update()} to move the game to the next generation.
+     * If the game state is paused, the above mentioned will be skipped
+     */
     public static synchronized void renderNext() {
         if (!isPaused) {
             updating = true;
@@ -41,11 +61,18 @@ public class LogicManager {
 
     private static Set<Point> checkedNeighbors = Collections.synchronizedSet(new HashSet<Point>());
 
+    /**
+     * Clears the cache of already checked neighbors. This method should be invoked after having executed the computation for each {@link Point}
+     */
     public static void clearCheckedCache() {
         checkedNeighbors.clear();
     }
 
-
+    /**
+     * Computes the state of a point in the next generation and checks if any neighbors of the given point will be born in the next generation.
+     * Changes will be forwarded to the {@link GridManager}.
+     * @param point The point for which to compute the above mentioned things
+     */
     public static void compute(Point point) {
         // get amount of neighbors
         int neighborAmount = 0;
@@ -79,12 +106,22 @@ public class LogicManager {
         }
     }
 
+    /**
+     * Pauses or unpauses the game based on the given argument.
+     * This method directly affects {@link LogicManager#renderNext()}
+     * and changes the appearance of the GUI by invoking {@link ToolBar#setPaused(boolean)} on the current {@link ToolBar} instance.
+     * @param paused
+     */
     public static synchronized void setPaused(boolean paused) {
         isPaused = paused;
         ToolBar.getCurrentInstance()
                 .setPaused(paused);
     }
 
+    /**
+     * Returns the current game state
+     * @return A boolean stating whether or not the game is paused
+     */
     public static synchronized boolean isPaused() {
         return isPaused;
     }
